063c4c85ab094f18b250c4c44402a87a15caf1ce
안로빈 <anrobin719@gmail.com>
2019-08-18 01:13:26 +0900

    [feat] Create markdownRender component rendering markdown to html, previewPaneContainer component using  ducks modular type

---
+++ blog-frontend/src/components/common/MarkdownRender/MarkdownRender.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import styles from './MarkdownRender.scss';
+import classNames from 'classnames/bind';
+
+import marked from 'marked';
+
+const cx = classNames.bind(styles);
+
+class MarkdownRender extends Component {
+    state = {
+        html: ''
+    }
+
+    renderMarkdown = () => {
+        const { markdown } = this.props;
+        if (!markdown) {
+            this.setState({ html: '' });
+            return;
+        }
+        this.setState({
+            html: marked(markdown, {
+                breaks: true, // 일반 엔터로 새 줄 입력
+                sanitize: true // 마크다운 내부 html 무시
+            })
+        });
+    }
+
+    // 서버사이드 렌더링에서도 마크다운 처리가 되도록 constructor에서도 구현
+    constructor(props) {
+        super(props);
+        const { markdown } = props;
+        this.state = {
+            html: markdown ? marked(props.markdown, { break: true, sanitize: true }) : ''
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // markdown 값이 변경되면 renderMarkdown 호출
+        if(prevProps.markdown !== this.props.markdown) {
+            this.renderMarkdown();
+        }
+    }
+
+    render() {
+        const { html } = this.state;
+        
+        // React에서 html을 렌더링하려면 객체를 만들어 내부에 __html 값을 설정해야 함
+        const markup = {
+            __html: html
+        }
+        // 그리고 dangerouslySetInnerHTML 값에 해당 객체를 넣어주면 됨
+        return (
+            <div className={cx('markdown-render')} dangerouslySetInnerHTML={markup} />
+        );
+    }
+}
+
+export default MarkdownRender;
