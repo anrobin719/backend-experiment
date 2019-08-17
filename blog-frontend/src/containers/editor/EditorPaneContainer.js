5205036606710f1bc1356ca22a8771af0b81ccc2
안로빈 <anrobin719@gmail.com>
2019-08-18 00:39:54 +0900

    [feat] Add redux module to editorPaneContainer component using ducks modular type

---
+++ blog-frontend/src/containers/editor/EditorPaneContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import EditorPane from '../../components/editor/EditorPane';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as editorActions from '../../modules/editor';
+
+
+class EditorPaneContainer extends Component {
+
+    handleChangeInput = ({name, value}) => {
+        const { EditorActions} = this.props;
+        EditorActions.changeInput({name, value});
+    }
+
+    render() {
+        const {title, tags, markdown} = this.props
+        return (
+            <EditorPane
+                title={title}
+                markdown={markdown}
+                tags={tags}
+                onChangeInput={this.handleChangeInput}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    title: state.editor.get('title'),
+    markdown: state.editor.get('markdown'),
+    tags: state.editor.get('tags')
+});
+const mapDispatchToProps = (dispatch) => ({
+    EditorActions: bindActionCreators(editorActions, dispatch)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(EditorPaneContainer);
