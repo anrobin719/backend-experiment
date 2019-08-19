c97835d56f6dde44ee712270dc59ec3f5d5972a0
안로빈 <anrobin719@gmail.com>
2019-08-19 10:52:18 +0900

    [feat] Show postList using query-string

--- blog-frontend/src/store/modules/list.js
+++ blog-frontend/src/store/modules/list.js
@@ -1,18 +1,33 @@
-// eslint-disable-next-line
 import { createAction, handleActions } from 'redux-actions';
 
-import { Map } from 'immutable';
-// eslint-disable-next-line
+import { Map, List, fromJS } from 'immutable';
 import { pender } from 'redux-pender';
 
+import * as api from '../../lib/api';
+
 // action types
+const GET_POST_LIST = 'list/GET_POST_LIST';
 
 // action creators
+export const getPostList = createAction(GET_POST_LIST, api.getPostList, meta => meta)
 
 // initial state
-const initialState = Map({});
+const initialState = Map({
+    posts: List(),
+    lastPage: null
+});
 
 // reducer
 export default handleActions({
+    ...pender({
+        type: GET_POST_LIST,
+        onSuccess: (state, action) => {
+            console.log(action.payload);
+            const { data: posts } = action.payload;
 
+            const lastPage = action.payload.headers['last-page'];
+            return state.set('posts', fromJS(posts))
+                        .set('lastPage', parseInt(lastPage, 10));
+        }
+    })
 }, initialState);
